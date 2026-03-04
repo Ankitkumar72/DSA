8e8beab997a6d1eab381519694c5ca90c6677032
Ankit Kumar <14420068m@gmail.com>
2026-03-04 14:17:34 +0530

    clean code

--- keep_alive.py
+++ keep_alive.py
@@ -9,7 +9,7 @@ def home():
     return "Bot is alive!"
 
 def run():
-    # Render assigns a dynamically generated port. We need to fetch it dynamically.
+   
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
 
